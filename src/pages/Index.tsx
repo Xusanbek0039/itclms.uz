86d3277e739224f548b82ec0619987bf102e8609
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-02-20 12:36:10 +0500

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Courses from "@/components/Courses";
+import Features from "@/components/Features";
+import Stats from "@/components/Stats";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <About />
+      <Courses />
+      <Features />
+      <Stats />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
